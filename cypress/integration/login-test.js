f5b7391126f36b4e3072095638beb435849d9fd2
A.Lazarev <a.lazarev@bpmonline.com>
2021-02-10 16:42:02 +0200

    add comments

--- cypress/integration/login-test.js
+++ cypress/integration/login-test.js
@@ -1,18 +1,26 @@
 describe('Login page', () => {
 	it('Login action using otp', () => {
 		cy.visit('http://int-web/TOTP_T_RC')
+
+		// Вводим логин
 		cy.get('[id="loginEdit-el"]')
 			.type('a.lazarev')
 			.should('have.value', 'a.lazarev')
+
+		// Вводим пароль
 		cy.get('[id="passwordEdit-el"]')
 			.type('Supervisor')
 			.should('have.value', 'Supervisor')
 
+		// Генерируем OTP код авторизации
 		cy.task("generateOTP", "7WU5IRXQ2EPJAFNMY7XTSGKBNIQEEJ4D").then(token => {
 			cy.get('[id="totpCodeEdit-el"]').type(token);
 		});
 
+		// Нажимаем кнопку логина
 		cy.get('[data-item-marker="btnLogin"]').click()
+
+		// Проверяем что мы вошли а приложение
 		cy.url().should('include', '/Nui/ViewModule.aspx')
 	})
 })
