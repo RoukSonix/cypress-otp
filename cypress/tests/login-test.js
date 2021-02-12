4dfd5067720d8711465dda14c80bdf274d235017
A.Lazarev <a.lazarev@bpmonline.com>
2021-02-12 16:18:45 +0200

    test refactoring

--- cypress/tests/login-test.js
+++ cypress/tests/login-test.js
@@ -1,4 +1,5 @@
 import { loginPage } from "../pages/login.page";
+import * as config from "../app-config.json"
 
 describe('Login page', () => {
 	beforeEach('Login Page tests', () => {
@@ -7,15 +8,12 @@ describe('Login page', () => {
 
 	it('Login action using otp', () => {
 		cy.visit('/') // Переход на страницу логина приложения
-		cy.get('[id="loginEdit-el"]').type('a.lazarev') // Ввод логина пользователя
-		cy.get('[id="passwordEdit-el"]').type('Supervisor') // Ввод пароля пользователя
-		//cy.task("generateOTP", "7WU5IRXQ2EPJAFNMY7XTSGKBNIQEEJ4D").then(token => {
-		//	cy.get('[id="totpCodeEdit-el"]').type(token);
-		//}); // Генерируем OTP код авторизации
+		cy.get('[id="loginEdit-el"]').type(config.UserName) // Ввод логина пользователя
+		cy.get('[id="passwordEdit-el"]').type(config.UserPassword) // Ввод пароля пользователя
 		loginPage.generateOtpPassword() // Генерируем OTP код авторизации
 		cy.task('generateOTP').then(token => {
 			cy.get('[id="totpCodeEdit-el"]').type(token);
-		})
+		}) // Ввод OTP токена авторизации
 		cy.get('[data-item-marker="btnLogin"]').click() // Нажимаем кнопку логина
 		cy.url().should('include', '/Nui/ViewModule.aspx') // Проверяем что мы вошли в приложение
 	})
