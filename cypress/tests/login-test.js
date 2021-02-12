d3176c3b1692f1456e79995d029d6faf101efcaa
A.Lazarev <a.lazarev@bpmonline.com>
2021-02-12 15:56:34 +0200

    add test documentation

--- cypress/tests/login-test.js
+++ cypress/tests/login-test.js
@@ -1,25 +1,18 @@
 
 
 describe('Login page', () => {
+	beforeEach('Login Page tests', () => {
+		cy.clearCookies() // Очистка cookie
+	})
+
 	it('Login action using otp', () => {
-		cy.clearCookies()
-		cy.visit('/')
-
-		// Вводим логин
-		cy.get('[id="loginEdit-el"]').type('a.lazarev')
-
-		// Вводим пароль
-		cy.get('[id="passwordEdit-el"]').type('Supervisor')
-
-		// Генерируем OTP код авторизации
+		cy.visit('/') // Переход на страницу логина приложения
+		cy.get('[id="loginEdit-el"]').type('a.lazarev') // Ввод логина пользователя
+		cy.get('[id="passwordEdit-el"]').type('Supervisor') // Ввод пароля пользователя
 		cy.task("generateOTP", "7WU5IRXQ2EPJAFNMY7XTSGKBNIQEEJ4D").then(token => {
 			cy.get('[id="totpCodeEdit-el"]').type(token);
-		});
-
-		// Нажимаем кнопку логина
-		cy.get('[data-item-marker="btnLogin"]').click()
-
-		// Проверяем что мы вошли в приложение
-		cy.url().should('include', '/Nui/ViewModule.aspx')
+		}); // Генерируем OTP код авторизации
+		cy.get('[data-item-marker="btnLogin"]').click() // Нажимаем кнопку логина
+		cy.url().should('include', '/Nui/ViewModule.aspx') // Проверяем что мы вошли в приложение
 	})
 })
